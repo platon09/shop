68025ca529d1ca68b3566be4c31b83848ee03ecf
platon09 <nis.platon@gmail.com>
2020-07-02 00:56:52 +0600

    Added new products

--- landing/views.py
+++ landing/views.py
@@ -8,5 +8,8 @@ def landing(request):
 
 
 def home(request):
-	products_images = ProductImage.objects.filter(is_active=True, is_main=True)
-	return render(request,'landing/home.html', locals())
+	products_images = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+	products_images_vegetables = products_images.filter(product__category__id=1)
+	products_images_fruits = products_images.filter(product__category__id=2)
+	products_images_meat = products_images.filter(product__category__id=3)
+	return render(request,'landing/home.html', locals())	
